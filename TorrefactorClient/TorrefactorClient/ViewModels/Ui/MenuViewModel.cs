7929d6be6a1e1b472d43135b96ecd756a11e4240
asch <a.schaal@evs.com>
2019-06-21 16:28:40 +0200

    Add Some new enhancement in navigation

--- TorrefactorClient/TorrefactorClient/ViewModels/Ui/MenuViewModel.cs
+++ TorrefactorClient/TorrefactorClient/ViewModels/Ui/MenuViewModel.cs
@@ -12,6 +12,7 @@ namespace TorrefactorClient.ViewModels.Ui
   public class MenuViewModel : BaseViewModel
   {
     private ObservableCollection<MenuFeature> _features;
+    private ObservableCollection<MenuFeature> _options;
     private MenuFeature _selectedFeature;
     private bool _isOpen;
 
@@ -25,6 +26,12 @@ namespace TorrefactorClient.ViewModels.Ui
       set { _features = value; Notify(); }
     }
 
+    public ObservableCollection<MenuFeature> Options
+    {
+      get { return _options; }
+      set { _options = value; Notify(); }
+    }
+
     public bool IsOpen
     {
       get { return _isOpen; }
@@ -36,8 +43,14 @@ namespace TorrefactorClient.ViewModels.Ui
       get { return _selectedFeature; }
       set
       {
+        if (value == _selectedFeature)
+          return;
+
         if (_selectedFeature != null)
           _selectedFeature.IsSelected = false;
+
+        _selectedFeature = null;
+        Notify();
 
         _selectedFeature = value;
 
@@ -57,9 +70,12 @@ namespace TorrefactorClient.ViewModels.Ui
       CommandClose = new CmdBinding(Close);
 
       _features = new ObservableCollection<MenuFeature>();
+      _options = new ObservableCollection<MenuFeature>();
 
       _features.Add(new MenuFeature { Icon = PackIconKind.ViewDashboard , Title = "Dashboard", View = new Home() });
       _features.Add(new MenuFeature { Icon = PackIconKind.ChartBubble , Title = "Charts", View = new Charts() });
+
+      _options.Add(new MenuFeature { Icon = PackIconKind.Settings, Title = "Settings", View = new Charts() });
     }
 
     private void Open()
