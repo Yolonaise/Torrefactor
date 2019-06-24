69dee3838134bb60423b3434838dac69ad7a06d6
asch <a.schaal@evs.com>
2019-06-24 17:04:07 +0200

    Adding the new API

---
+++ TorrefactorApi/TorrefactorApi/Context/Model/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TorrefactorApi.Context
+{
+  public class User
+  {
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    [Key]
+    public Guid Id { get; set; }
+    public string Username { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+  }
+}
---
+++ TorrefactorApi/TorrefactorApi/Repository/Model/User.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TorrefactorApi.Repository.Model
+{
+  public class User
+  {
+  }
+}
